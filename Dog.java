e40b68338f4d688e62da4f5a3facbaa4ac5b7401
Dhamo77 <dhamotharancr7@gmail.com>
2024-06-20 11:22:51 +0530

    Update Dog.java

--- Dog.java
+++ Dog.java
@@ -26,6 +26,7 @@ public class Dog {
     public String toString() {
         return "Name : "+name+", Breed : "+breed;
     }
+     //main function 
     public static void main(String[] args) {
         Dog d1 =new Dog("Buddy", "Golden Retriever");
         Dog d2 =new Dog("Charlie", "Bulldog");
