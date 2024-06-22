2c41c9519412f39e8911ab8fff8eb1b61fd217f5
Dhamo77 <dhamotharancr7@gamil.com>
2024-06-22 14:50:03 +0530

    add the nearest_of_zero file in repo

---
+++ nearest_of_zero.java
@@ -0,0 +1,15 @@
+public class nearest_of_zero {
+        public static void main(String[] args) {
+            double[] mat ={-5,-1.7,2,12,1.7};
+            System.out.println(find(mat));
+        }
+        public static double find(double[] arr){
+            double near =Integer.MAX_VALUE;
+            for (int i=1;i<arr.length;i++){
+                double  min=arr[i];
+                if(Math.abs(min)<Math.abs(near)||( (Math.abs(near)==Math.abs(min))&&min>near))
+                    near = min;
+            }
+            return near;
+        }
+}
